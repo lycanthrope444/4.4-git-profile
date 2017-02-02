9e0c7282552123a7d948d6ab72e5cd80245dca2d
Nathan <nathanstarwalt@gmail.com>
2017-02-02 17:56:33 -0500

    Template work & more AJAX work

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,8 +5,6 @@ require('bootstrap-sass');
 var personalInfoUrl = "https://api.github.com/users/lycanthrope444";
 var repoInfoUrl = "https://api.github.com/users/lycanthrope444/repos";
 var githubtoken = require('./gitapikey.js');
-var repoSource = $('#repo-template').html();
-var repoTemplate = Handlebars.compile(repoSource);
 
 // Send auth token to github if token is provided
 if(githubtoken !== undefined){
@@ -17,20 +15,33 @@ if(githubtoken !== undefined){
   });
 }
 
+//Used to populate Personal Info
 $.ajax(personalInfoUrl).done(function(data){
   console.log(data);
-  // var userInfo = {
-  //   profilePic:
-  // }
-
+  var source = $('#user-links').html();
+  var template = Handlebars.compile(source);
+  var userInfo = {
+    userOverview: data.html_url,
+    repoNum: data.public_repos,
+    // starNum: data.
+    followerNum: data.followers,
+    followingNum: data.following
+  };
+  $('.staticbar').append(template(userInfo));
 });
 
+// Used to populate Repository Info
 $.ajax(repoInfoUrl).done(function(data){
   console.log(data);
+  var repoSource = $('#repo-template').html();
+  var repoTemplate = Handlebars.compile(repoSource);
   _.each(data, function(info){
     var repoInfo = {
+      name: info.name,
       url: info.url,
       language: info.language
     };
+    // console.log(repoInfo);
+    $(".repo-list").append(repoTemplate(repoInfo));
   });
 });
